c769e796a2f0c6fb40d91ec1f3129958f58473d6
yigitsametolmez <ygtsmt@outlook.com>
2023-06-29 01:28:02 +0300

    deneme

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/ygtsmt/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/ygtsmt/Downloads/amasya_app-main"
+export "FLUTTER_APPLICATION_PATH=/Users/ygtsmt/Downloads/amasyaapp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=2.0.1"
-export "FLUTTER_BUILD_NUMBER=201"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1.0.0"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
